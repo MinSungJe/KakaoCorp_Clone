75554648531229a2e3c3014146255051450ac017
Minje Sung <minje813@dgu.ac.kr>
2024-10-11 17:23:08 +0900

    [style] 동영상 컴포넌트 화 (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 
+import VideoComponent from '@/components/components'
+
 export default function Home() {
   return (
     <div>
-      <p>메인페이지 준비완료</p>
-      <video width="100%" muted autoPlay loop>
-        <source src="/4c2201fa019200001.mp4" type="video/mp4" />
-        브라우저에서 동영상을 지원하지 않습니다.
-      </video>
+      <p>메인페이지 준비완료!</p>
+      <VideoComponent />
     </div>
   )
 }
