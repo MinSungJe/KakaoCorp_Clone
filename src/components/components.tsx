3f92cfdac5dbf1a6263b5538ba22a669c74a81a3
Minje Sung <minje813@dgu.ac.kr>
2024-10-11 17:23:08 +0900

    [style] 동영상 컴포넌트 화 (#1)

---
+++ src/components/components.tsx
@@ -0,0 +1,8 @@
+export default function VideoComponent() {
+  return (
+    <video width="100%" muted autoPlay loop>
+      <source src="/4c2201fa019200001.mp4" type="video/mp4" />
+      브라우저에서 동영상을 지원하지 않습니다.
+    </video>
+  )
+}
