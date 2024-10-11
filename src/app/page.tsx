cd60b20b65f028f261f3202671b4b39bebaa78c9
Minje Sung <minje813@dgu.ac.kr>
2024-10-12 03:22:43 +0900

    Feat/#1. 컴포넌트 이름 변경 및 src 인자 추가 (#4)
    * [feat] src 인자 추가 (#1)
    
    * [rename] 컴포넌트명 수정 (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <p>메인페이지 준비완료!</p>
-      <VideoComponent />
+      <VideoComponent src="/4c2201fa019200001.mp4" />
     </div>
   )
 }
--- src/components/VideoComponent.tsx
+++ src/components/VideoComponent.tsx
@@ -1,7 +1,7 @@
-export default function VideoComponent() {
+export default function VideoComponent({ src }: { src: string }) {
   return (
     <video width="100%" muted autoPlay loop>
-      <source src="/4c2201fa019200001.mp4" type="video/mp4" />
+      <source src={src} type="video/mp4" />
       브라우저에서 동영상을 지원하지 않습니다.
     </video>
   )
