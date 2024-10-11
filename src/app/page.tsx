4e5cc1a5c74d9325a22ccd9df7a3435e5ea0ae89
Minje Sung <minje813@dgu.ac.kr>
2024-10-11 17:17:33 +0900

    [feat] 동영상 불러오는 video 태그 작성 (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import styles from './page.module.css'
+/* eslint-disable jsx-a11y/media-has-caption */
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div>
       <p>메인페이지 준비완료</p>
+      <video width="100%" muted autoPlay loop>
+        <source src="/4c2201fa019200001.mp4" type="video/mp4" />
+        브라우저에서 동영상을 지원하지 않습니다.
+      </video>
     </div>
   )
 }
