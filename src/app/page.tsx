53fc2040ca49502f727b8061804e0d8f32469e38
Minje Sung <minje813@dgu.ac.kr>
2024-10-13 01:30:27 +0900

    Feat/#1. 컴포넌트에 src 인자 추가 및 이름 변경 (#6)
    * [feat] src 인자 추가 (#1)
    
    * [rename] 컴포넌트명 수정 (#1)
    
    * [fix] 경로 수정 (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 
+import VideoComponent from '@/components/VideoComponent'
 import CarouselComponent from '@/components/CarouselComponent'
 
 export default function Home() {
-  return <CarouselComponent />
+  return (
+    <div>
+      <p>메인페이지 준비완료!</p>
+      <CarouselComponent />
+      <VideoComponent src="/4c2201fa019200001.mp4" />
+    </div>
+  )
 }
