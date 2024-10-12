0db22dd7f84a888360f221f6efb7555d3c4d41b4
Minje Sung <minje813@dgu.ac.kr>
2024-10-12 23:26:37 +0900

    [feat] api 생성 및 연동 (#2)

--- src/components/CarouselComponent.tsx
+++ src/components/CarouselComponent.tsx
@@ -3,12 +3,14 @@
 'use client'
 
 import useCarouselScroll from '@/hooks/useCarouselScroll'
+import useCarouselCardData from '@/hooks/useCarouselCardData'
 import { UpImageCard } from './CardComponents'
 import S from './styles/CarouselComponent.module.css'
 
 export default function CarouselComponent() {
   const [handleMouseDown, handleMouseMove, handleMouseUp, handleMouseLeave] =
     useCarouselScroll()
+  const data = useCarouselCardData()
 
   return (
     <div
@@ -18,36 +20,15 @@ export default function CarouselComponent() {
       onMouseUp={handleMouseUp} // 마우스 클릭 끝날 때
       onMouseLeave={handleMouseLeave} // 마우스가 캐러셀을 벗어날 때
     >
-      <UpImageCard
-        title="제목1"
-        description="설명1"
-        tag={['태', '그']}
-        src="/card-thumbnail.webp"
-      />
-      <UpImageCard
-        title="제목2"
-        description="설명2"
-        tag={['태', '그']}
-        src="/card-thumbnail.webp"
-      />
-      <UpImageCard
-        title="제목3"
-        description="설명3"
-        tag={['태', '그']}
-        src="/card-thumbnail.webp"
-      />
-      <UpImageCard
-        title="제목4"
-        description="설명4"
-        tag={['태', '그']}
-        src="/card-thumbnail.webp"
-      />
-      <UpImageCard
-        title="제목5"
-        description="설명5"
-        tag={['태', '그']}
-        src="/card-thumbnail.webp"
-      />
+      {data.map((element) => (
+        <UpImageCard
+          key=""
+          title={element.title}
+          description={element.description}
+          tag={element.tag}
+          src={element.src}
+        />
+      ))}
     </div>
   )
 }
