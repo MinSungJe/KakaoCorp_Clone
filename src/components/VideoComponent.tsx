7bdafd02b932cfb21aff19b22903a8d9e1b9d85d
Minje Sung <minje813@dgu.ac.kr>
2024-10-14 00:39:35 +0900

    [feat] Video에 border-radius 추가 (#1) (#9)

--- src/components/VideoComponent.tsx
+++ src/components/VideoComponent.tsx
@@ -1,6 +1,6 @@
 export default function VideoComponent({ src }: { src: string }) {
   return (
-    <video height="100%" muted autoPlay loop>
+    <video height="100%" muted autoPlay loop style={{ borderRadius: '10px' }}>
       <source src={src} type="video/mp4" />
       브라우저에서 동영상을 지원하지 않습니다.
     </video>
