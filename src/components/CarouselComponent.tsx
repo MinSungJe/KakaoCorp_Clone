1ae3c68656a2367fa475f650316eb491b805522a
Minje Sung <minje813@dgu.ac.kr>
2024-10-13 21:03:57 +0900

    [feat] 캐러셀 title 추가 (#2)

--- src/components/CarouselComponent.tsx
+++ src/components/CarouselComponent.tsx
@@ -13,22 +13,33 @@ export default function CarouselComponent() {
   const data = useCarouselCardData()
 
   return (
-    <div
-      className={S.carousel}
-      onMouseDown={handleMouseDown} // 마우스 클릭 시작
-      onMouseMove={handleMouseMove} // 마우스 이동 중
-      onMouseUp={handleMouseUp} // 마우스 클릭 끝날 때
-      onMouseLeave={handleMouseLeave} // 마우스가 캐러셀을 벗어날 때
-    >
-      {data.map((element) => (
-        <UpImageCard
-          key=""
-          title={element.title}
-          description={element.description}
-          tag={element.tag}
-          src={element.src}
-        />
-      ))}
-    </div>
+    <>
+      <div className={S.title}>
+        <div className={S.titleContent}>
+          <div className={S.titleGIF} />
+          <p className={S.titleText}>
+            카카오와 카카오 그룹이 함께 축적해온 이야기들
+          </p>
+        </div>
+        <div className={S.carouselStopBtn} />
+      </div>
+      <div
+        className={S.carousel}
+        onMouseDown={handleMouseDown} // 마우스 클릭 시작
+        onMouseMove={handleMouseMove} // 마우스 이동 중
+        onMouseUp={handleMouseUp} // 마우스 클릭 끝날 때
+        onMouseLeave={handleMouseLeave} // 마우스가 캐러셀을 벗어날 때
+      >
+        {data.map((element) => (
+          <UpImageCard
+            key=""
+            title={element.title}
+            description={element.description}
+            tag={element.tag}
+            src={element.src}
+          />
+        ))}
+      </div>
+    </>
   )
 }
