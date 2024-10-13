4011da228d5cac574d44869165097bbd916fe789
Minje Sung <minje813@dgu.ac.kr>
2024-10-13 22:34:28 +0900

    [feat] 동영상 옆 CardComponent 추가 (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 
-import VideoComponent from '@/components/VideoComponent'
 import CarouselComponent from '@/components/CarouselComponent'
+import TitleVideoComponent from '@/components/TitleVideoComponent'
 
 export default function Home() {
   return (
     <div>
       <p>메인페이지 준비완료!</p>
+      <TitleVideoComponent />
       <CarouselComponent />
-      <VideoComponent src="/4c2201fa019200001.mp4" />
     </div>
   )
 }
