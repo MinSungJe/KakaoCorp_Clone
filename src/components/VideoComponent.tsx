a90f705566ed42ba8b4bf892c11c74ab7cbaffad
Minje Sung <minje813@dgu.ac.kr>
2024-10-15 02:29:13 +0900

    [fix] Video border-radius 적용 안되는 버그 수정 (#12)

--- src/components/VideoComponent.tsx
+++ src/components/VideoComponent.tsx
@@ -2,13 +2,7 @@ import S from './styles/TitleVideoComponent.module.css'
 
 export default function VideoComponent({ src }: { src: string }) {
   return (
-    <video
-      className={S.Video}
-      muted
-      autoPlay
-      loop
-      style={{ borderRadius: '10px' }}
-    >
+    <video className={S.Video} muted autoPlay loop>
       <source src={src} type="video/mp4" />
       브라우저에서 동영상을 지원하지 않습니다.
     </video>
